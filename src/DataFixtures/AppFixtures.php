d2ccd3f2b2134f65a19276e8dd000e76f104ff8d
melissa-studi <melissa_bazir@yahoo.fr>
2023-10-09 15:40:42 +0200

    adding Faker for reviews

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,20 +2,30 @@
 
 namespace App\DataFixtures;
 
+use Faker\Generator;
+use Faker\Factory;
 use App\Entity\Review;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Generator;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
-    
+    /**
+     *
+     * @var Generator
+     */
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }   
     public function load(ObjectManager $manager): void
     {
         for ($i=1; $i <= 50 ; $i++) {
             $review = new Review();
-            $review->setName('Laurent' . $i)
-            ->setReviewText('Très professionnel')
+            $review->setName($this->faker->name())
+            ->setReviewText($this->faker->text())
             ->setScore(mt_rand(3, 4))
             ->setApproved(False);
 
