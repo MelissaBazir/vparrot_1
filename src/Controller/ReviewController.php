95fd30fc02950b89aeced0e97492ff6d33e22f30
melissa-studi <melissa_bazir@yahoo.fr>
2023-10-13 21:03:58 +0200

    redefinition of review CRUD

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -2,9 +2,8 @@
 
 namespace App\Controller;
 
-
 use App\Entity\Review;
-use App\Form\ReviewType;
+use App\Form\Review1Type;
 use App\Repository\ReviewRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
@@ -13,92 +12,77 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/review')]
 class ReviewController extends AbstractController
 {
-    /**
-     * This controller displays all the reviews (approved or not)
-     *
-     * @param ReviewRepository $repo
-     * @param PaginatorInterface $paginator
-     * @param Request $request
-     * @return Response
-     */
-    #[Route('/review', name: 'review.index', methods: ['GET'])]
-    public function index(ReviewRepository $repo, PaginatorInterface $paginator, Request $request): Response
+    #[Route('/', name: 'app_review_index', methods: ['GET'])]
+    public function index(ReviewRepository $reviewRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        //pagination
         $reviews = $paginator->paginate(
-        $repo->findAll(), 
-        $request->query->getInt('page', 1), /*page number*/
-        9 /*limit per page*/
+            $reviewRepository->findAll(), 
+            $request->query->getInt('page', 1), /*page number*/
+            9 /*limit per page*/
         );
-        return $this->render('pages/review/index.html.twig', [
-            'reviews' => $reviews
+        
+        return $this->render('review/index.html.twig', [
+            'reviews' => $reviews,
         ]);
     }
 
-    /**
-     * This controller is used to add a review
-     *
-     * @param Request $request
-     * @param EntityManagerInterface $manager
-     * @return Response
-     */
-    #[Route('/review/new', 'review.new', methods: ['GET', 'POST'])]
-    public function new(
-        Request $request,
-        EntityManagerInterface $manager) : Response
+    #[Route('/new', name: 'app_review_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $review = new Review();
-        $form = $this->createForm(ReviewType::class, $review);
-        
+        $form = $this->createForm(Review1Type::class, $review);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
-            $review = $form->getData();
 
-            $manager->persist($review);
-            $manager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($review);
+            $entityManager->flush();
 
-            // Message to inform the user review has been successfully sent
-            $this->addFlash(
-                'success',
-                'Votre avis a bien été envoyé'
-            );
-
+            return $this->redirectToRoute('app_review_index', [], Response::HTTP_SEE_OTHER);
         }
-        
-        return $this->render('pages/review/new.html.twig', [
-            'form' => $form->createView()
+
+        return $this->renderForm('review/new.html.twig', [
+            'review' => $review,
+            'form' => $form,
         ]);
     }
 
-    #[Route('/review/edition/{id}', 'review.edit', methods: ['GET', 'POST'])]
-    public function edit(
-        ReviewRepository $repo, 
-        int $id, 
-        Request $request, 
-        EntityManagerInterface $manager): Response
+    #[Route('/{id}', name: 'app_review_show', methods: ['GET'])]
+    public function show(Review $review): Response
     {
-        $review = $repo->findOneBy(["id" => $id]);
-        
-        $form = $this->createForm(ReviewType::class, $review);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $review = $form->getData();
-
-            $manager->persist($review);
-            $manager->flush();
-
-            // Message to inform the review has been successfully modified
-            $this->addFlash(
-                'success',
-                'L\'avis a bien été modifié'
-            );
-        }
-        
-        return $this->render('pages/review/edit.html.twig', [
-            'form' => $form->createView()
+        return $this->render('review/show.html.twig', [
+            'review' => $review,
         ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_review_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Review $review, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(Review1Type::class, $review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_review_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('review/edit.html.twig', [
+            'review' => $review,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_review_delete', methods: ['POST'])]
+    public function delete(Request $request, Review $review, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$review->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($review);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_review_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -4,93 +4,25 @@ namespace App\Form;
 
 use App\Entity\Review;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
-
-class ReviewType extends AbstractType
+class Review1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'minlength' => '3',
-                    'maxlength' => '32'
-                ],
-                'label' => 'Votre nom',
-                'label_attr' =>  [
-                    'class' => 'form-label mt-4'
-                ],
-                'constraints' => [
-                    new Assert\Length(['min' => 2, 'max' => 50]),
-                    new Assert\NotBlank()
-                ]
-            ])
-            ->add('reviewText', TextareaType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'maxlength' => '255'
-                ],
-                'label' => 'Votre avis',
-                'label_attr' =>  [
-                    'class' => 'form-label mt-4'
-                ],
-                'constraints' => [
-                    new Assert\Length(['max' => 255]),
-                    new Assert\NotBlank()
-                ]
-            ])
-            ->add('score', IntegerType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'min' => 1,
-                    'max' => 4
-                ],
-                'label' => 'Votre note',
-                'label_attr' =>  [
-                    'class' => 'form-label mt-4'
-                ],
-                'constraints' => [
-                    new Assert\GreaterThanOrEqual(1),
-                    new Assert\LessThanOrEqual(4),
-                    new Assert\NotBlank()
-                ]
-            ])
-            // ->add('approved', CheckboxType::class, [
-            // 'attr' => ['value' => 'false']
-            // ])
-            ->add('submit', SubmitType::class, [
-                'attr' => [
-                    'class' => 'btn btn-primary mt-4 mb-4'
-                ],
-                'label' => 'Envoyer mon avis'
-            ]);
-
-        // submit label to fix
-        // $labelSubmit = "";
-        // switch ($options['route']) {
-        //     case 'review.new':
-        //         $labelSubmit = "Ajouter un avis";
-        //         break;
-        //     case 'review.edit':
-        //         $labelSubmit = "Modifier l'avis'";
-        //         break;
-        // }
+            ->add('name')
+            ->add('reviewText')
+            ->add('score')
+            ->add('approved')
+            ->add('createdAt')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // 'route' => null,
             'data_class' => Review::class,
         ]);
     }
